93d6e0da7a30cf95ab9e2146e2745cdb3c2c5726
Zach <zachisaunders12@gmail.com>
2024-07-08 11:18:11 -0400

    Revert "Added some ui elements -- definitely not complete yet."
    This reverts commit cdc0d41827aee6e3855603aac551f556a173a362.
    
    Previous commit broke CMake because of my inability to use gitmergetool

--- main.cpp
+++ main.cpp
@@ -1,28 +1,10 @@
 #include "mainwindow.h"
 
 #include <QApplication>
-<<<<<<< HEAD
-=======
-#include <QLocale>
-#include <QTranslator>
->>>>>>> e93b799 (Added some ui elements -- definitely not complete yet.)
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-<<<<<<< HEAD
-=======
-
-    QTranslator translator;
-    const QStringList uiLanguages = QLocale::system().uiLanguages();
-    for (const QString &locale : uiLanguages) {
-        const QString baseName = "QWebT_" + QLocale(locale).name();
-        if (translator.load(":/i18n/" + baseName)) {
-            a.installTranslator(&translator);
-            break;
-        }
-    }
->>>>>>> e93b799 (Added some ui elements -- definitely not complete yet.)
     MainWindow w;
     w.show();
     return a.exec();
